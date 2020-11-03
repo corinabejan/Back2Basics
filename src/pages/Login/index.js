bfc3a11f1200e0c5be2405f647992b3899210bf7
Emnetdegafe <emnetdegafe@gmail.com>
2020-11-04 00:28:23 +0100

    login page styling

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,56 +1,60 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import firebase from 'firebase'
+import "./login.css"
+import { Link } from "react-router-dom"
 
-export default function LoginScreen(){
+export default function LoginScreen() {
     const [email, set_Email] = useState("")
-    const [password, set_Password] =useState("")
+    const [password, set_Password] = useState("")
 
-    function sendEmail(e){
+    function sendEmail(e) {
         e.preventDefault()
 
         console.log("Event test", email, password)
 
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(e){
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (e) {
             var errorCode = e.code;
             var errorMessage = e.message;
             console.log(`Error, ${errorCode}, ${errorMessage}`)
         })
         set_Password("")
         set_Email("")
-        
+
     }
 
 
-    return(
-        <div>
+    return (
+        <div id="login-container">
             <h1>
-                Login Screen
+                Login
             </h1>
             <form>
-                <label
-                    >
-                    Email:
-                </label>
-                <input
-                    type="email"
-                    id="user_email"
-                    placeholder="example@gmail.com"
-                    value={email}
-                    onChange={(e) => set_Email(e.target.value)}
-                />
                 <label>
-                    Password:
+                    Email: {" "}
+                    <input
+                        type="email"
+                        id="user_email"
+                        placeholder="example@gmail.com"
+                        value={email}
+                        onChange={(e) => set_Email(e.target.value)}
+                    />
                 </label>
-                <input
-                    type="password"
-                    id="user_password"
-                    value={password}
-                    onChange={(e) => set_Password(e.target.value)}
-                />
+                <label>
+                    Password:{" "}
+                    <input
+                        type="password"
+                        id="user_password"
+                        value={password}
+                        onChange={(e) => set_Password(e.target.value)}
+                    />
+                </label>
                 <button
                     type="submit"
                     onClick={sendEmail}
-                >Submit</button>
+                >Login</button>
+                <Link to="/signup">
+                    <p>sign up here</p>
+                </Link>
             </form>
         </div>
     )
