8e5461621d06b8708d0eb16d34eee40a79072462
Emnetdegafe <emnetdegafe@gmail.com>
2020-11-03 23:19:08 +0100

    added some grid styling

--- src/pages/Mentor/Mentors.js
+++ src/pages/Mentor/Mentors.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import "./mentors.css"
+import profile from "./profile.jpg"
+import { Link } from "react-router-dom"
 
 const findTrainess = "Find Your Mentee"
 
 
 export default function Mentors() {
     return (
-        <div>
-            <img className="profile-img" src = "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="profile">
-            </img>
-            <div className="intro-container">
-                <p>Credientials: BA in budiness Admin</p>
-                <p>Intor Text: </p>
-            </div>
-            <button>
-                {findTrainess}
-            </button>
+        <div id="mentor-container">
+            <p className="welcome-text">welcome back lili</p>
+            <section className="profile-info">
+                <img src={profile} className="bio-img" alt="profile" />
+                <div className="intro-container">
+                    <h3>Lili Jonson: BA in business Adminstration</h3>
+                    <p>Intor Text: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, </p>
+                </div>
 
+            </section>
+            <Link to="/mentee">
+                <button>
+                    {findTrainess}
+                </button>
+            </Link>
         </div>
     )
 }
