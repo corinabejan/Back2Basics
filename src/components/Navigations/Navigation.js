677e3ea065154ffd1996d29b59b65d51466683ca
Emnetdegafe <emnetdegafe@gmail.com>
2020-11-03 16:35:33 +0100

    Added navigation bar

---
+++ src/components/Navigations/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navigations.css"
+
+export default function Navigation() {
+    return (
+        <section className="nav-bar">
+            <p>Mentor</p>
+            <p>Mentees</p>
+            <p>Teacher</p>
+            <p>Student</p>
+        </section>
+    )
+}
