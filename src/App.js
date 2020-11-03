5ba6abdae85a86a20d899726cbe3fe08c7379968
Emnetdegafe <emnetdegafe@gmail.com>
2020-11-03 22:42:08 +0100

    rendering mentor and homepages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from "react-router-dom"
 import LoginScreen from "../src/pages/Login/index"
 import SignUp from "../src/pages/Sign-Up/index"
 import Navigation from './components/Navigations/Navigation';
+import HomePage from "./pages/HomePage/HomePage"
+import Mentors from './pages/Mentor/Mentors';
 
 
 function App() {
@@ -10,19 +12,26 @@ function App() {
     <div className="App">
       <Navigation />
       <header className="App-header">
-
-        Back 2 Basics
+        <Switch>
+          <Route
+            exact path="/"
+            component={HomePage}
+          />
+          <Route
+            exact path="/mentor"
+            component={Mentors}
+          />
+          <Route
+            exact path="/login"
+            component={LoginScreen}
+          />
+          <Route
+            exact path="/signup"
+            component={SignUp}
+          />
+        </Switch>
       </header>
-      <Switch>
-        <Route
-          exact path="/login"
-          component={LoginScreen}
-        />
-        <Route
-          exact path="/signup"
-          component={SignUp}
-        />
-      </Switch>
+
     </div>
   );
 }
