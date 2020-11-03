8341880e87ef165b621c263c32b0c661bcf4ca9a
Emnetdegafe <emnetdegafe@gmail.com>
2020-11-03 16:36:43 +0100

    Render navigation bar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import './App.css';
 import { Switch, Route } from "react-router-dom"
 import LoginScreen from "../src/pages/Login/index"
 import SignUp from "../src/pages/Sign-Up/index"
+import Navigation from './components/Navigations/Navigation';
+
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
+
         Back 2 Basics
       </header>
       <Switch>
-          <Route
-            exact path="/login"
-            component={LoginScreen}
-          />
-          <Route
-            exact path="/signup"
-            component={SignUp}
-          />
+        <Route
+          exact path="/login"
+          component={LoginScreen}
+        />
+        <Route
+          exact path="/signup"
+          component={SignUp}
+        />
       </Switch>
     </div>
   );
