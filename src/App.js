0a8b2f3ed2f81b2c706b1f4d5c2b69186659a580
Emnetdegafe <emnetdegafe@gmail.com>
2020-11-05 00:49:32 +0100

    render mentor list and some styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import SignUp from "../src/pages/Sign-Up/index"
 import Navigation from './components/Navigations/Navigation';
 import HomePage from "./pages/HomePage/HomePage"
 import Mentors from './pages/Mentor/Mentors';
+import Mentee from './pages/Mentee/Mentee-list/Mentee'
+import MenteeDetail from './pages/Mentee/Mentee-detail'
+import MentorList from './pages/Mentor/Mentor-list';
 
 
 function App() {
@@ -18,8 +21,20 @@ function App() {
             component={HomePage}
           />
           <Route
-            exact path="/mentor"
+            exact path="/mentors"
+            component={MentorList}
+          />
+          <Route
+            exact path="/mentors/:id"
             component={Mentors}
+          />
+          <Route
+            exact path="/mentee"
+            component={Mentee}
+          />
+          <Route
+            exact path="/mentee/:id"
+            component={MenteeDetail}
           />
           <Route
             exact path="/login"
