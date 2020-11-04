c9355b56585e96161c9c70f2874926230474b772
DarianRushworth <darianerrolrushworth@gmail.com>
2020-11-04 15:03:01 +0100

    Teachers fetch setup test to come

--- src/pages/Teacher/index.js
+++ src/pages/Teacher/index.js
@@ -1,19 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import firebase from "firebase"
 
 
 export default function Teacher(){
     const db = firebase.firestore()
+    const auth = firebase.auth()
+    const [teacherData, set_TeacherData] = useState({})
 
-    
+    const docRef = db.collection("users").doc(auth.currentUser.uid)
+
+    docRef.onSnapshot(function(document){
+        document.exists
+        ? set_TeacherData(document.data())
+        : console.log("No Document found")
+    })
+
+
     return(
         <div>
             <h1>
                 Teacher Page
             </h1>
             <h2>
-                Teacher Name
+                {teacherData.user_name}
             </h2>
             <p>
                 Image as avatar
