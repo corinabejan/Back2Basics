76a0dffebf2385e98784b4c9a80f991b799f5666
corinabejan <corinabe93@gmail.com>
2020-11-05 22:39:06 +0100

    displaying lessons on the student dashboard

--- src/pages/CustomLessons/index.js
+++ src/pages/CustomLessons/index.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
-import firebase from "firebase"
-import "./index.css"
+import firebase from "firebase";
+import "./index.css";
 
-const skillsArray = ["Math", "Reading", "Writing", 
-        "Social Studies", "Enviromental", "Creative Arts", "Technology"]
+const skillsArray = [
+  "Math",
+  "Reading",
+  "Writing",
+  "Social Studies",
+  "Enviromental",
+  "Creative Arts",
+  "Technology",
+];
 
 export default function CustomLesson() {
   const [loading, setLoading] = useState(false);
   const [video, setVideo] = useState("");
-  const [educationType, set_EducationType] = useState("")
-  const db = firebase.firestore()
-  const auth = firebase.auth()
+  const [educationType, set_EducationType] = useState("");
+  const db = firebase.firestore();
+  const auth = firebase.auth();
 
   const uploadVideo = async (e) => {
     e.preventDefault();
@@ -36,34 +43,37 @@ export default function CustomLesson() {
     setLoading(false);
   };
 
-  function sendData(e){
-      e.preventDefault()
-    db.collection("custom_lessons").doc(`Custom: ${auth.currentUser.uid}`)
-    .set({
+  function sendData(e) {
+    e.preventDefault();
+    db.collection("custom_lessons")
+      .doc(`Custom: ${auth.currentUser.uid}`)
+      .set({
         lesson_type: educationType,
-        video_url: video
-    })
-    .then(function(){
-        var popUp = document.getElementById("myPopup")
-        popUp.classList.toggle("show")
-        console.log("success")
-    })
-    .catch(function(error){
-        console.log("Error", error)
-    })
+        video_url: video,
+      })
+      .then(function () {
+        var popUp = document.getElementById("myPopup");
+        popUp.classList.toggle("show");
+        console.log("success");
+      })
+      .catch(function (error) {
+        console.log("Error", error);
+      });
   }
 
-  const optionSetter = skillsArray.map(skill => {
-                        return (
-                            <option
-                                key={skill}
-                                value={skill}
-                                onClick={(e) => {
-                                    set_EducationType(e.target.value)}
-                                }
-                            >{skill}</option>
-                            )
-                        })
+  const optionSetter = skillsArray.map((skill) => {
+    return (
+      <option
+        key={skill}
+        value={skill}
+        onClick={(e) => {
+          set_EducationType(e.target.value);
+        }}
+      >
+        {skill}
+      </option>
+    );
+  });
 
   console.log(video);
 
@@ -77,29 +87,27 @@ export default function CustomLesson() {
           <video controls src={video} style={{ width: "500px" }} />
         )}
         <form>
-            <input
+          <input
             type="file"
             name="file"
             placeholder="Upload a Video"
             onChange={uploadVideo}
-            />
-            <label>
-                Education Type:
-            </label>
-            <select>
-                <option>--Choose-Skill--</option>
-                {optionSetter}
-            </select>
-            <button
-                type="submit"
-                onClick={(e) => sendData(e)}
-            >
-                Create Lesson
-            </button>
+          />
+          <label>Education Type:</label>
+          <select>
+            <option>--Choose-Skill--</option>
+            {optionSetter}
+          </select>
+          <button type="submit" onClick={(e) => sendData(e)}>
+            Create Lesson
+          </button>
         </form>
-        <div class="popup" onClick="showPop()">Click me!
-            <span class="popuptext" id="myPopup">Lesson Added</span>
-        </div> 
+        <div class="popup" onClick="showPop()">
+          Click me!
+          <span class="popuptext" id="myPopup">
+            Lesson Added
+          </span>
+        </div>
       </div>
     </>
   );
