026da02ba8257b728eadc320543a88fa42f6b8e1
corinabejan <corinabe93@gmail.com>
2020-11-06 09:06:23 +0100

    text added to homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,20 +1,57 @@
-import React from 'react'
-import "./homepages.css"
+import React from "react";
+import "./homepages.css";
 export default function HomePage() {
-    return (
-        <div className="home-container">
-            <div className="text-container-1">
-                <h3>Who we are</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
-            </div>
-            <div className="text-container-2">
-                <h3>What we do</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
-            </div>
-            <div className="text-container-3">
-                <h3>How it works</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="home-container">
+      <div className="text-container-1">
+        <h3>Who we are</h3>
+        <p>
+          Three human beings: Emnet, Darian and Corina. Three different
+          countries: Ethiopia, South Africa and Romania. Three different
+          cultures, and three different backgrounds. Three motivated people that
+          experienced lack of education and gender equality first hand, and that
+          hope they can make a change, even if it is a small one, any kind of
+          change can make a big difference in the life of so many others. We are
+          simply humans with a big desire to bring children around the world a
+          chance to study and to find greatness. We humbly hope that our
+          platform can do that.
+        </p>
+      </div>
+      <div className="text-container-2">
+        <h3>What we do</h3>
+        <p>
+          Back2Basics is a platform that is trying to facilitate studying for
+          children, and bring awarness in gender equality. The basic idea behind
+          this platform is to help narrow the gap in education and gender
+          equality. We have set out to achieve this by setting out educational
+          videos from Beginner to Advanced, with the option of making your own
+          custom video as a teacher. It is a platform where children/adults can
+          get great mentorship on how to help handle and improve their current
+          challenges. We have gone with a more femine theme to appeal more to
+          the female audience as we see this is the biggest gap to shrink.
+        </p>
+      </div>
+      <div className="text-container-3">
+        <h3>How it works</h3>
+        <p>
+          Back2Basics gives a chance to everyone to participate, wheter is a
+          Teacher, a Student, a Mentor or a Men-Tee. Everyone can Sign Up for
+          free on the platform on the specific role that they want to have. Each
+          memmber has its own Dashboard that will include all the necessary
+          information to interact with the platform. As a Teacher, you will be
+          able to add a regular lesson or a custom one, everything being video
+          based. As a Student you will have access to all available courses, you
+          will be able to rewatch everything as many times as you want, and you
+          also have a whiteboard where you can practice what you just learned.
+          As a Mentor you will provide a description of yourself and see a list
+          of people that are in need of a mentor, and as a Men-tee you will be
+          able to see the profiles of the mentors and you can choose which
+          mentor you believe is a good fit for your needs. It is simple to use,
+          and very straightforward and in the future we also want to add a tech
+          support line where we can answer all the questions that there are
+          about how to navigate through the platform. Enjoy!!!!
+        </p>
+      </div>
+    </div>
+  );
 }
