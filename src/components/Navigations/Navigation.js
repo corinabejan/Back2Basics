ed118cde98b7a8ab730faa552b7de36ed80ada66
Emnetdegafe <emnetdegafe@gmail.com>
2020-11-06 08:33:22 +0100

    redirect to own pages

--- src/components/Navigations/Navigation.js
+++ src/components/Navigations/Navigation.js
@@ -5,10 +5,10 @@ export default function Navigation() {
     return (
         // <nav>
         <ul id="navigation-bar">
-            <li> <a href="/login">Mentor</a></li>
-            <li><a href="/login">Mentee</a></li>
-            <li>< a href="/login">Teacher</a></li>
-            <li><a href="/login">Student</a></li>
+            <li> <a href="/mentors">Mentor</a></li>
+            <li><a href="/mentee">Mentee</a></li>
+            <li>< a href="/teacher">Teacher</a></li>
+            <li><a href="/student">Student</a></li>
         </ul>
         // {/* </nav> */}
     )
